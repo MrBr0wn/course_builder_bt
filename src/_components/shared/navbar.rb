8d9b20a5aa385da30b7cf27b3f45f9b236c1d68f
Mr. Brown <gitcherrypick.work@gmail.com>
2022-05-06 17:41:39 +0500

    Remove all

--- src/_components/shared/navbar.rb
+++
@@ -1,9 +0,0 @@
-module Shared
-  class Navbar < ViewComponent::Base
-    include Bridgetown::ViewComponentHelpers
-
-    def initialize(metadata:, resource:)
-      @metadata, @resource = metadata, resource
-    end
-  end
-end
